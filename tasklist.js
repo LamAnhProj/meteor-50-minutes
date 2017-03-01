8157c9ea67a073d7f59c6d80b024a485f0da2418
Anh Lam <lilwiterose@yahoo.com>
2017-03-01 04:51:37 -1000

    Task 2: Tasklist setup

--- tasklist.js
+++ tasklist.js
@@ -1,11 +1,14 @@
-/**
- * Created by Xla on 3/1/17.
- */
 Tasks = new Mongo.Collection('tasks');
-if(Meteor.isClient){
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
 
 }
 
-if(Meteor.isServer){
-
-}
