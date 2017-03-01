feae5dad0f8083c05dd6242f047e931dbd2201ae
Anh Lam <lilwiterose@yahoo.com>
2017-03-01 05:05:33 -1000

    Task 5: subscriptions

--- tasklist.js
+++ tasklist.js
@@ -1,6 +1,8 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
     tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -25,7 +27,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  })
 }
 
 Meteor.methods({
@@ -42,4 +46,3 @@ Meteor.methods({
     Tasks.remove(taskId);
   }
 })
-
